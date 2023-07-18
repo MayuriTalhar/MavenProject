f76dd04fca4b25e91397f89c4ab2444195a3cf15
Mayuri <mayuri.talhar95@gmail.com>
2023-07-18 10:45:55 +0100

    Initial commit

---
+++ mavenProject/src/test/java/com/trainingProject/mavenProject/MavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingProject.mavenProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
